2b64b7c5bc4afd037379f807f048ce2e6ce5e033
Gabriel Oliveira <goliveira@lopesrio.intra>
2024-06-04 15:15:54 -0300

    fim da secao 4

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css'
+import { useState } from 'react'
 import MyComponent from './components/MyComponent'
+import Title from './components/Title'
 
 function App() {
+
+  const n = 51
+  const [name] = useState("Mateus")
+
+  // Classe dinâmica
+  const redTitle = true
+
   return (
     <div>
       {/* css global */}
@@ -13,6 +22,17 @@ function App() {
       <p style={{color: "white", padding: "25px", borderTop: "2px solid cyan"}}>
         Este elemento foi estilizado de forma inline
       </p>
+
+      {/* CSS inline dinâmico */}
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS Dinânico</h2>
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS Dinânico 2</h2>
+      <h2 style={name === "Mateus" ? ({color: "green", backgroundColor:"#000"}) : null}>CSS Dinâmico</h2>
+
+      {/* Classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este Título tem Classe Dinâmica
+      </h2>
+      <Title />
     </div>
   )
 }
